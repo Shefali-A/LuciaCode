511f522ed5e0ea0e0ea8eb021be74a56e90e640a
Sonia <sonia@Sonias-MacBook-Air-2.local>
2018-06-26 10:05:30 -0400

    lesson8

--- Lesson 1/venv/Lesson1.py
+++ Lesson 1/venv/Lesson1.py
@@ -23,23 +23,59 @@
 # 4.Put on your shoes
 # 5.Walk out the door
 
+#
+# width = 5
+# length = 4
+# area = width * length
+# #print area
+#
+# name = "lila"
+# numberOfJeans = 5
+# priceOfJeans = 10
+# taxRate = 0.10
+# #print name, "will spend $", numberOfJeans * priceOfJeans * (taxRate+ 1), "on", str(numberOfJeans), "pairs of jeans"
+#
+#
+# Me = 17 - 15 + 12
+# Jenny = 7 * 3
+# print str(Jenny + Me)
+#
+# inputIsDivisible = 10
+# if inputIsDivisible % 5 == 0 and inputIsDivisible % 3 == 0:
+#     print "Divisible by both"
+# elif inputIsDivisible % 5 == 0:
+#     print "Divisible by 5"
+# elif inputIsDivisible % 3 == 0:
+#     print "Divisible by 3"
+# else:
+#     print "Divisible by neither"
 
-width = 5
-length = 4
-area = width * length
-#print area
 
-name = "lila"
-numberOfJeans = 5
-priceOfJeans = 10
-taxRate = 0.10
-#print name, "will spend $", numberOfJeans * priceOfJeans * (taxRate+ 1), "on", str(numberOfJeans), "pairs of jeans"
+# start = 10
+# end = 20
+#
+# for i in range(start, end, 2):
+#     print i
+#
+# i = start
+# while i < end:
+#     print i
+#     i = i + 2
 
+start = 2
+end = 100
+for i in range(start, end, 2):
+    print i
 
-Me = 17 - 15 + 12
-Jenny = 7 * 3
-print str(Jenny + Me)
+# The current year is (a)
+# The year I was born is (b)
+# My current age is (c)
+# The age I am turning this year is (result)
 
-inputIsDivisible = 
-if inputIsDivisible = 3
-    print ""
+def age(a, b, c):
+    result = a - b
+    print "The current year is " + str(a) + "\nThe year I was born is " + str(b) + "\nMy current age is " + str(c) + "\nThe age I am turning this year is " + str(result)
+
+age(2018, 2004, 13)
+
+age(2022, 2000, 21)
